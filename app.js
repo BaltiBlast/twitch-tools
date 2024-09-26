630e995abd5040dd943b12ebbbd9c349eef3a9e9
BaltiBlast <fougeray.florian@gmail.com>
2024-09-26 22:13:15 +0200

    fix .env variable

--- app.js
+++ app.js
@@ -23,7 +23,7 @@ app.use(
     resave: false,
     saveUninitialized: false,
     cookie: {
-      secure: process.env.NODE_ENV === "production",
+      secure: process.env.COOKIE_SECURE === "production",
       maxAge: 1000 * 60 * 60 * 24,
     },
   })
